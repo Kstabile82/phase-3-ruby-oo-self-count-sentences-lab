bf923613d03d1b13e836b5b82dd0a269fc9d2c39
KarinaStabile <karina.stabile@gmail.com>
2022-03-09 21:17:29 -0500

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?(".")
   end
 
   def question?
-
+   self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sent = 0 
+    punct = [".", "?", "!"]
+     self.chars.each.with_index do |char, index|
+      if punct.include?(char) && !punct.include?(self[index + 1])
+      sent += 1
+    end
+  end
+    sent
   end
 end
